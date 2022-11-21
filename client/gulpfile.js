2316a7ed7895ba3db9473dcf40d418697d6434fa
ali hatem <AliHatem>
2022-11-21 14:15:16 +0200

    create header comp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -5,5 +5,5 @@ const gulpSass = require("gulp-sass");
 const sass = gulpSass(require("sass"));
 
 gulp.task("sass", async function () {
-  gulp.src("src/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
+  gulp.src("src/componets/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
 });
---
+++ client/src/componets/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../css/Header/Header.css";
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>Header</h1>
+      </header>
+    </>
+  );
+};
+
+export default Header;
